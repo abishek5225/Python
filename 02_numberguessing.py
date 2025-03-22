fc8d0a87f5b91616f8307f8c13ea5d6cdf78f632
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-22 20:37:44 +0545

    updated

--- 02_numberguessing.py
+++ 02_numberguessing.py
@@ -1,16 +1,20 @@
 import random
 
-secretnum = random.randint(1,10)
-attempts = 3
+secretnum = random.randint(1, 10)
+attempts = 4
 
 while attempts > 0:
-     guess = int(input("\nGuess a number ({attempts} left):"))
+    guess = int(input(f"\nGuess a number ({attempts} left): "))
 
-if(guess == secretnum):
-    print("You guessed the number!")
-    break
-
+    if guess == secretnum:
+        print(" You guessed the number!")
+        break
     elif guess < secretnum:
-        print("guess higher! Try again.")
-else:
-    print("Guess lower! Try again.")
+        print(" Guess higher! Try again.")
+    else:
+        print("Guess lower! Try again.")
+
+    attempts -= 1
+
+    if attempts == 0:
+        print(f"\n💀 Game over! The number was {secretnum}")
