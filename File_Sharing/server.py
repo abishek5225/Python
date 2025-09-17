04dcd99b0910a530cb909eb2cd571659d509fa56
Abishek  Chaulagain <abishekchaulagain5225@gmail.com>
2025-09-17 12:51:40 +0545

    updated

---
+++ File_Sharing/server.py
@@ -0,0 +1,14 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.socket.SOCK_STREAM)
+server_socket.bind(("127.0.0.1", 12345))
+
+server_socket.listen()
+print("Server is listening")
+
+client_socket, client_address = server_socket.accept()
+print(f"connected to {client_address}")
+data = client_socket.recv(1024).decode()
+print("client messege: ", data)
+
+client_socket.send("Hello from Server!".encode())
