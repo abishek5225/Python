1121ceb3f0c6984896f8611fa26e68482f86d6bd
Abishek  Chaulagain <abishekchaulagain5225@gmail.com>
2025-09-17 13:00:14 +0545

    updated

--- File_Sharing/client.py
+++ File_Sharing/client.py
@@ -1,3 +1,11 @@
 import socket
 
-client_socket = socket.socket()
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(("127.0.0.1", 12345))
+
+client_socket.send("Hello from client" .encode())
+
+data = client_socket.recv(1024).decode()
+print("Received from server:", data)
+
+client_socket.close()
